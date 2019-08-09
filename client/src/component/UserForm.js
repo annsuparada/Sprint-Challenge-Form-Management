4ca94e974e438b30fb458c27498a993d639b6e06
Suparada Saitalae <ann.suparada@gmail.com>
2019-08-09 12:14:13 -0500

    add Yub

--- client/src/component/UserForm.js
+++ client/src/component/UserForm.js
@@ -3,14 +3,22 @@ import axios from 'axios';
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 
-const UserForm = () => {
+const UserForm = ({ errors, touched }) => {
 
     return(
         <div>
             <h1>Login</h1>
             <Form>
                 <Field type="text" name="username" placeholeder="Username" />
+                {touched.username && errors.username && (
+                    <p className="error">{errors.username}</p>
+                )}
+
                 <Field type="password" name="password" placeholeder="Password" />
+                {touched.password && errors.password && (
+                    <p className="error">{errors.password}</p>
+                )}
+
                 <button type="submit">Submit</button>
             </Form>
         </div>
@@ -24,6 +32,12 @@ const FormikUserForm = withFormik({
             passwoed: password || ''
         }
     },
+
+    ValidationSchema: Yup.object().shape({
+        username: Yup.string().required(),
+        password: Yup.string().required()
+    }),
+
 
     handleSubmit(values, { setStatus }) {
         axios
