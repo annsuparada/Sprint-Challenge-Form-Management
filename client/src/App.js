b7eda03fbd37a92c9a40aef36d544b75de75ad12
Suparada Saitalae <ann.suparada@gmail.com>
2019-08-09 10:29:12 -0500

    firt commit, create UserForm component

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import UserForm from './component/UserForm'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Login Form</h1>
+      </header>
+
+      <UserForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/component/UserForm.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
+
+const UserForm = () => {
+    return(
+        <div>
+            <h1>Login</h1>
+        </div>
+    )
+}
+
+export default UserForm;
